7d699e61d6b59cfca81be340bec69eba6ef72822
Charles Chou <Charles_Chou@htc.com>
2021-07-25 22:53:40 +0800

    merge two file into processUrl.go

--- services/processUrl.go
+++ services/processUrl.go
@@ -45,3 +45,30 @@ func getExpiredAt(t int64, now int64) int64 {
 	}
 	return t
 }
+
+func GetOriginalUrl(req *modelsReq.GetOriginalUrlReq, res *modelsRes.GetOriginalUrlRes) (int64, *modelsRes.GetOriginalUrlRes, models.UrlProcessingError) {
+	if !req.Validate() {
+		return 400, res, models.BadRequestError
+	}
+
+	queryUrlReq := db.QueryUrlData{}
+	queryUrlReq.ShorteningUrl = req.ShorteningUrl
+	queryUrlReq.ExpiredAtEffectiveStart = utils.GetCurrentMillisecondTimestamp()
+	total, resp, err := db.UrlDB.QueryUrlData(&queryUrlReq)
+	if err != nil {
+		return 500, res, models.InternalServerError
+	}
+	if total > 1 {
+		return 409, res, models.UrlProcessingError{40901, "duplicate original url for one shortening url"}
+	}
+	if total == 0 {
+		return 204, res, models.NotFoundError
+	}
+	urlData := resp[0]
+	res.OriginalUrl = urlData.OriginalUrl
+	res.ShorteningUrl = urlData.ShorteningUrl
+	res.ExpiredAt = urlData.ExpiredAt
+	res.CreatedAt = urlData.CreatedAt
+
+	return 200, res, models.NoError
+}
